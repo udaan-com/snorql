a86dc84cda53c67c089ad3f27e85c29b4846b7de
Prakul Jain <prakul.jain@udaan.com>
2021-12-30 10:48:47 +0530

    POC Working v1

--- snorql-framework/src/test/kotlin/com/udaan/snorql/framework/job/tests/JobManagerTest.kt
+++ snorql-framework/src/test/kotlin/com/udaan/snorql/framework/job/tests/JobManagerTest.kt
@@ -16,41 +16,41 @@ import java.time.ZoneId
 
 class JobManagerTests {
 
-    private val jobManager = JobManager()
-
-    @Test
-    fun jobSchedulingTest() {
-        SqlMetricManager.addMetric("performance_activeQueries", ActiveQueriesMetric())
-        val mockConnection: Connection = mock()
-        SqlMetricManager.setConnection(mockConnection)
-        jobManager.startScheduler()
-        val triggerConfig1 = JobTriggerConfig(
-            watchIntervalInSeconds = 2,
-            startFrom = Timestamp.from(LocalDateTime.now().toInstant(ZoneId.systemDefault().rules.getOffset(
-                LocalDateTime.now())))
-        )
-        val triggerConfig2 = JobTriggerConfig(
-            watchIntervalInSeconds = 3,
-            startFrom = Timestamp.from(LocalDateTime.now().toInstant(ZoneId.systemDefault().rules.getOffset(
-                LocalDateTime.now())))
-        )
-        val triggerConfig3 = JobTriggerConfig(
-            watchIntervalInSeconds = 6,
-            startFrom = Timestamp.from(LocalDateTime.now().toInstant(ZoneId.systemDefault().rules.getOffset(
-                LocalDateTime.now())))
-        )
-        val metricInput1 = ActualMetricInput(
-            metricId = "performance_activeQueries",
-            databaseName = "uddevsql/db-test"
-        )
-        val metricInput2 = ActualMetricInput(
-            metricId = "performance_activeQueries",
-            databaseName = "uddevsql/db-dev"
-        )
-        val metricInput3 = ActualMetricInput(
-            metricId = "performance_activeQueries",
-            databaseName = "uddevsql/db-test"
-        )
+//    private val jobManager = JobManager
+//
+//    @Test
+//    fun jobSchedulingTest() {}
+//        SqlMetricManager.addMetric("performance_activeQueries", ActiveQueriesMetric())
+//        val mockConnection: Connection = mock()
+//        SqlMetricManager.setConnection(mockConnection)
+////        jobManager.startScheduler()
+//        val triggerConfig1 = JobTriggerConfig(
+//            watchIntervalInSeconds = 2,
+//            startFrom = Timestamp.from(LocalDateTime.now().toInstant(ZoneId.systemDefault().rules.getOffset(
+//                LocalDateTime.now())))
+//        )
+//        val triggerConfig2 = JobTriggerConfig(
+//            watchIntervalInSeconds = 3,
+//            startFrom = Timestamp.from(LocalDateTime.now().toInstant(ZoneId.systemDefault().rules.getOffset(
+//                LocalDateTime.now())))
+//        )
+//        val triggerConfig3 = JobTriggerConfig(
+//            watchIntervalInSeconds = 6,
+//            startFrom = Timestamp.from(LocalDateTime.now().toInstant(ZoneId.systemDefault().rules.getOffset(
+//                LocalDateTime.now())))
+//        )
+//        val metricInput1 = ActualMetricInput(
+//            metricId = "performance_activeQueries",
+//            databaseName = "uddevsql/db-test"
+//        )
+//        val metricInput2 = ActualMetricInput(
+//            metricId = "performance_activeQueries",
+//            databaseName = "uddevsql/db-dev"
+//        )
+//        val metricInput3 = ActualMetricInput(
+//            metricId = "performance_activeQueries",
+//            databaseName = "uddevsql/db-test"
+//        )
 
 //        whenever(SqlMetricManager.queryExecutor.persistHistoricalData(anyString(), any())).thenAnswer{
 //            val storageId: String = it.arguments[0] as String
@@ -64,22 +64,22 @@ class JobManagerTests {
 //        whenever(mockManager.queryExecutor.persistHistoricalData(anyString(), any())).thenAnswer {
 //            println("Following data has been saved in storageBucketId ${it.arguments[0]}\n${it.arguments[1]}")
 //        }
-        jobManager.addJob<ActualMetricInput, ActualMetricOutput, IMetricRecommendation>(triggerConfig1,
-            metricInput1)
-        jobManager.addJob<ActualMetricInput, ActualMetricOutput, IMetricRecommendation>(triggerConfig2,
-            metricInput2)
-        jobManager.addJob<ActualMetricInput, ActualMetricOutput, IMetricRecommendation>(triggerConfig3,
-            metricInput1)
-        println("Printing the triggers configured...")
-        jobManager.getAllMonitoringTriggers().forEach {
-            println(it)
-        }
-        while (true) {
-            Thread.sleep(20_000)
-        }
-        jobManager.triggerJob()
-        jobManager.triggerJob()
-    }
+//        jobManager.addJob<ActualMetricInput, ActualMetricOutput, IMetricRecommendation>(triggerConfig1,
+//            metricInput1)
+//        jobManager.addJob<ActualMetricInput, ActualMetricOutput, IMetricRecommendation>(triggerConfig2,
+//            metricInput2)
+//        jobManager.addJob<ActualMetricInput, ActualMetricOutput, IMetricRecommendation>(triggerConfig3,
+//            metricInput1)
+//        println("Printing the triggers configured...")
+//        jobManager.getAllMonitoringTriggers().forEach {
+//            println(it)
+//        }
+//        while (true) {
+//            Thread.sleep(20_000)
+//        }
+//        jobManager.triggerJob()
+//        jobManager.triggerJob()
+//    }
 
 //    @Test // Used to remove all the triggers
 //    fun removeAllTriggers() {
@@ -92,6 +92,4 @@ class JobManagerTests {
 //        jobManager.startScheduler()
 //        jobManager.removeEverything()
 //    }
-
-
 }
