7216c9a949c9141601c7f9adb512b8762cb88833
Prakul Jain <prakul.jain@udaan.com>
2022-03-17 15:08:48 +0530

    Detekt fixes

--- snorql-extensions/src/main/kotlin/com/udaan/snorql/extensions/session/SessionEnums.kt
+++ snorql-extensions/src/main/kotlin/com/udaan/snorql/extensions/session/SessionEnums.kt
@@ -2,7 +2,7 @@ package com.udaan.snorql.extensions.session
 
 import com.udaan.snorql.framework.IMtericId
 
-enum class SessionEnums(private val metricId:String): IMtericId {
+enum class SessionEnums(private val metricId:String) : IMtericId {
 
     /**
      * Session Locks Metric ID mapping
