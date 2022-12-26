750de8ba459264a104d92111a66fd021181daef8
Prakul Jain <prakul.jain@udaan.com>
2022-12-26 19:49:31 +0530

    1. Updated the project version to 0.10-SNAPSHOT 2. Made recommendationsRequired as `open` in MetricInput DTO. 3. Added IndexFragmentationMetric.kt and IndexFragmentationModel.kt and made entry in PerformanceEnums.kt 4. Added IndexFragmentationMetric config in sql-monitoring-conf.json
    Signed-off-by: Prakul Jain <prakul.jain@udaan.com>

---
+++ snorql-extensions/src/main/kotlin/com/udaan/snorql/extensions/performance/metrics/IndexFragmentationMetric.kt
@@ -0,0 +1,91 @@
+package com.udaan.snorql.extensions.performance.metrics
+
+import com.udaan.snorql.extensions.performance.models.IndexFragmentationDTO
+import com.udaan.snorql.extensions.performance.models.IndexFragmentationInput
+import com.udaan.snorql.extensions.performance.models.IndexFragmentationRecommendation
+import com.udaan.snorql.extensions.performance.models.IndexFragmentationResult
+import com.udaan.snorql.extensions.performance.models.IndexInfo
+import com.udaan.snorql.framework.SQLMonitoringConfigException
+import com.udaan.snorql.framework.metric.IMetric
+import com.udaan.snorql.framework.metric.SqlMetricManager
+import com.udaan.snorql.framework.models.IMetricResult
+import com.udaan.snorql.framework.models.MetricConfig
+import com.udaan.snorql.framework.models.MetricInput
+import com.udaan.snorql.framework.models.MetricOutput
+
+class IndexFragmentationMetric :
+    IMetric<IndexFragmentationInput, IndexFragmentationResult, IndexFragmentationRecommendation> {
+
+    override fun getMetricResult(
+        metricInput: IndexFragmentationInput,
+        metricConfig: MetricConfig
+    ): IndexFragmentationResult {
+        val query =
+            metricConfig.queries["main"]
+                ?: throw SQLMonitoringConfigException("SQL config query [main] not found under config [${metricInput.metricId}]")
+        val paramMap = mapOf("modeParam" to metricInput.mode.name)
+        val result =
+            SqlMetricManager.queryExecutor.execute<IndexFragmentationDTO>(
+                metricInput.databaseName,
+                query,
+                paramMap
+            )
+        return IndexFragmentationResult(result)
+    }
+
+    override fun getMetricRecommendations(
+        metricInput: IndexFragmentationInput,
+        metricResult: IndexFragmentationResult
+    ): IndexFragmentationRecommendation? {
+        if (metricInput.recommendationRequired) {
+            val indexesToReorganise: MutableList<IndexInfo> = mutableListOf()
+            val indexesToRebuild: MutableList<IndexInfo> = mutableListOf()
+            metricResult.queryList.forEach { idxFrag ->
+                if (idxFrag.avgFragmentationInPercent in 10.0..30.0) {
+                    indexesToReorganise.add(
+                        IndexInfo(
+                            idxFrag.schemaName,
+                            idxFrag.objectName,
+                            idxFrag.indexName,
+                            idxFrag.indexType,
+                            idxFrag.pageCount
+                        )
+                    )
+                } else if (idxFrag.avgFragmentationInPercent > 30) {
+                    indexesToRebuild.add(
+                        IndexInfo(
+                            idxFrag.schemaName,
+                            idxFrag.objectName,
+                            idxFrag.indexName,
+                            idxFrag.indexType,
+                            idxFrag.pageCount
+                        )
+                    )
+                }
+            }
+            return IndexFragmentationRecommendation(
+                indexesToRebuild = indexesToRebuild,
+                indexesToReorganise = indexesToReorganise
+            )
+        }
+        return null
+    }
+
+    override fun getMetricResponseMetadata(
+        metricInput: IndexFragmentationInput,
+        metricOutput: MetricOutput<IndexFragmentationResult, IndexFragmentationRecommendation>
+    ): Map<String, Any>? {
+        val responseMetadata = mutableMapOf<String, Any>()
+        val metricConfig = getMetricConfig(metricInput.metricId)
+        val query = metricConfig.queries["main"]
+        responseMetadata["underlyingQueries"] = listOf(query)
+        responseMetadata["referenceDocumentation"] = metricConfig.referenceDoc
+        responseMetadata["description"] = metricConfig.description
+        responseMetadata["supportsHistorical"] = metricConfig.supportsHistorical
+        return responseMetadata
+    }
+
+    override fun saveMetricResult(metricInput: MetricInput, result: IMetricResult) {
+        TODO("Not yet implemented")
+    }
+}
